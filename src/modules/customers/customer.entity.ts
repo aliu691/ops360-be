0edf3fff4fe2caef86c13d28586c7b497947cad9
Aliu691 <aliuom1996@gmail.com>
2026-02-06 13:26:42 +0100

    fix

--- src/modules/customers/customer.entity.ts
+++ src/modules/customers/customer.entity.ts
@@ -26,9 +26,9 @@ export class Customer {
   })
   contacts?: CustomerContact[];
 
-  @CreateDateColumn()
+  @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
-  @UpdateDateColumn()
+  @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
 }
