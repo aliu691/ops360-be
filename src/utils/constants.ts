7850475209495f26776aa768891d5fda49b2519c
Aliu691 <aliuom1996@gmail.com>
2026-03-01 17:07:51 +0100

    refactor weekly evaluator

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const roleRegex =
-  /(director|manager|officer|staff|engineer|consultant|lead|coordinator|head|administrator|ceo|cto|cfo|vp|vice|principal|team|procurement|it|person|people|technical|ciso)/i;
+  /\b(director|manager|officer|staff|engineer|consultant|lead|coordinator|head|administrator|ceo|cto|cfo|vp|vice|principal|team|procurement|department|technical|ciso)\b/i;
