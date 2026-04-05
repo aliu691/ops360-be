38cc7a62eede55d97973d505e50c8de540e7a732
Aliu691 <aliuom1996@gmail.com>
2026-04-05 20:45:20 +0100

    refactor

--- src/modules/reporting/pdf.service.ts
+++ src/modules/reporting/pdf.service.ts
@@ -5,6 +5,8 @@ export class PdfService {
     const browser = await puppeteer.launch({
       headless: true,
 
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
+
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
